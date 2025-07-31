10f2450cd43a5e0e7c82221eb269dea4870745c7
Shaun Gao <shaungao80@gmail.com>
2025-07-30 21:35:15 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import "./styles.css";
-import "./handleCategory.js";
-import "./handleTodo.js";
-import { displayTodo, displayCategory } from './display.js';
-import { createTodo, createCategory } from './createObjects.js';
-import { addCategoryToDom } from "./addToDom.js";
+
 
